30f315b75bd06144af4193fcc4aaeb947f2d38bc
kmhk.lu <kmhk.lu@aol.com>
2017-02-23 21:16:00 +0200

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Com on 23/02/2017.
+//  Copyright © 2017 Com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Com on 23/02/2017.
+//  Copyright © 2017 Com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
